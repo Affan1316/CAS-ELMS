8fbeba7891b0e03d4bb4a1433e7513b6ecf67f8c
Affan1316 <stopkillinginghza@gmail.com>
2025-07-16 20:21:48 +0500

    Resolved merge conflict in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cas_app_main/src/core/theme/app_theme.dart';
-<<<<<<< HEAD
-import 'package:flutter_cas_app_main/src/features/course_catalog/presentation/pages/__course_catalog_screen_state.dart';
-=======
 import 'package:flutter_cas_app_main/src/features/categories_and_login_screen/presentation/bloc/login_onboarding_bloc.dart';
->>>>>>> 01d65bc471f94e1a622bf413bc2487cb0027858f
-import 'package:flutter_cas_app_main/src/features/elms_landing/presentation/pages/elms_landing_page.dart';
 
 void main() {
   runApp(const MyApp());
