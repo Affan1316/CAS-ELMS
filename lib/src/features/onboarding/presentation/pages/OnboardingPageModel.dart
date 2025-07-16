82a3386fed4fc4becd3416b095cf29c9fd2d3543
mahameer <mahameermuhammad@gmail.com>
2025-07-16 20:26:56 +0500

    adding on-boarding feature

---
+++ lib/src/features/onboarding/presentation/pages/OnboardingPageModel.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPageModel {
+  final String title;
+  final String description;
+  final String imagePath;
+  final Color color;
+
+  OnboardingPageModel({
+    required this.title,
+    required this.description,
+    required this.imagePath,
+    required this.color,
+  });
+}
