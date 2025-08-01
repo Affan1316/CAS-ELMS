92596b50b9f42e638f78a9a75410e4f217e7621b
saimriaz8 <askfromsaim@gmail.com>
2025-08-01 17:05:01 +0500

    Adding fee defaulter

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cas_app_main/src/core/theme/app_theme.dart';
 import 'package:flutter_cas_app_main/src/features/Chat_Page/presentation/bloc/chat_page_bloc.dart';
+import 'package:flutter_cas_app_main/src/features/alumni_page/presentation/pages/year_selector_page.dart';
 import 'package:flutter_cas_app_main/src/features/categories_and_login_screen/presentation/bloc/login_onboarding_bloc.dart';
 import 'package:flutter_cas_app_main/src/features/onboarding/presentation/pages/onboarding_screen.dart';
 import 'package:responsive_ui_kit/responsive_ui_kit.dart';
