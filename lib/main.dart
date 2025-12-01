56a8d8c56033c988ac505b38a690089bed7d09d4
Muhammad Aqib <aqibtufail7546@gmail.com>
2025-12-01 17:18:35 +0500

    splash

--- lib/main.dart
+++ lib/main.dart
@@ -131,7 +131,7 @@ class MyApp extends StatelessWidget {
           theme: AppTheme.lightTheme,
           darkTheme: AppTheme.darkTheme,
           themeMode: ThemeMode.system,
-          home: SuperAdminDashboard(),
+          home: SplashScreen(),
           // home: StudentTimeTrackerPage(),
         ),
       ),
